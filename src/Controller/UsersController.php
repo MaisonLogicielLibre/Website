0ac3ba97f83a404f5f2ce42f752a323f0822de2e
Raphael <solarus45@gmail.com>
2015-09-18 13:28:09 -0400

    Added permissions and isAuthorized to all controller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -248,6 +248,7 @@ class UsersController extends AppController
     {
         $this->request->allowMethod(['post', 'delete']);
         $user = $this->Users->get($id);
+
         if ($this->Users->delete($user)) {
             $this->Flash->success(__('The user has been deleted.'));
         } else {
