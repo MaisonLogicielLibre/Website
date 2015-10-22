f5cc3f4cbf8ccb995500c801df200ab55f84058c
Simon Begin <simonbegin90@gmail.com>
2015-10-22 13:39:30 -0400

    Add pending validation label on project that need validations

--- webroot/js/projects/index.js
+++ webroot/js/projects/index.js
@@ -3,11 +3,17 @@ $(document).ready(function () {
     // When the table draw the columns
     table.fnSettings().aoRowCallback.push({
         "fn": function (nRow, aData, iDisplayIndex) {
+            console.log(aData);
             $('td:eq(0)', nRow).html(
                 $('<a/>', {
                     href: projectUrl + '/' + aData['id'],
                     text: aData['name']
-                })
+                })[0].outerHTML
+                + (!aData['accepted'] ?  ' ' +
+                $('<span/>', {
+                    class: 'label label-warning label-as-badge',
+                    text: validationTxt
+                })[0].outerHTML : '')
             );
             $('td:eq(1)', nRow).html(
                 $('<a/>', {
