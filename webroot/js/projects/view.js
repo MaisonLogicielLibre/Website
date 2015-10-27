1e628a6b6df41870e9731809a9461ea2b0ba62ad
Simon Begin <simonbegin90@gmail.com>
2015-10-27 09:03:09 -0400

    Add new collumn

--- webroot/js/projects/view.js
+++ webroot/js/projects/view.js
@@ -10,8 +10,23 @@ $(document).ready(function () {
                     text: aData['name']
                 })
             );
+            $('td:eq(1)', nRow).html(
+                sessionTr[aData['session']]
+            );
+
+            $('td:eq(2)', nRow).html(
+                lengthTr[aData['length']]
+            );
+
+            if (aData.type_missions != null) {
+                var type_missions = '';
+                $(aData.type_missions).each(function (i, v) {
+                    type_missions += typeMissionsTr[v.id - 1] + ', ';
+                });
+                $('td:eq(3)', nRow).html(type_missions.slice(0, -2));
+            }
             if (aData.user != null) {
-                $('td:eq(1)', nRow).html(
+                $('td:eq(4)', nRow).html(
                     aData.user['firstName'] + ' ' + aData.user['lastName']
                 )
             }
