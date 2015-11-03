4a037515c9c5a885426bde5b5e7d33c2592f4a4a
RignonNoel <rignon.noel@openmailbox.org>
2015-11-03 11:21:58 -0500

    Create helper for wiki

--- src/View/Helper/WikiHelper.php
+++ src/View/Helper/WikiHelper.php
@@ -1,11 +1,26 @@
 <?php
+/**
+ * Wiki Helper
+ *
+ * @category Helper
+ * @package  Website
+ * @author   CakePHP <cakephp@email.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://cakephp.org CakePHP(tm) Project
+ */
 namespace App\View\Helper;
 
 use Cake\View\Helper;
 use Cake\View\View;
 
 /**
- * Wiki helper
+ * Wiki Helper
+ *
+ * @category Helper
+ * @package  Website
+ * @author   CakePHP <cakephp@email.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://cakephp.org CakePHP(tm) Project
  */
 class WikiHelper extends Helper
 {
@@ -17,4 +32,37 @@ class WikiHelper extends Helper
      */
     protected $defaultConfig = [];
 
+    /**
+     * BuildLink method.
+     *
+     * @param string $pageName pageName
+     *
+     * @return string $url $url
+     */
+    public function buildLink($pageName)
+    {
+        $lang = $this->request->session()->read('lang');
+        $lang = substr($lang, 0, 2);
+
+        $url = "http://wiki.maisonlogiciellibre.org/doku.php?id=" . $lang . ":" . $pageName;
+
+        return $url;
+    }
+
+    /**
+     * AddHelper method.
+     *
+     * @param string $pageName pageName
+     *
+     * @return string $link $link
+     */
+    public function addHelper($pageName)
+    {
+<<<<<<< Updated upstream
+        $link = "<a href='" . $this->buildLink($pageName) . "'><i class='fa fa-question-circle'></i></a>";
+=======
+        $link = "<a title='". __('Go to wiki') ."' href='" . $this->buildLink($pageName) . "'><i class='fa fa-question-circle'></i></a>";
+>>>>>>> Stashed changes
+        return $link;
+    }
 }
