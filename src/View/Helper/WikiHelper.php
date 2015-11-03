93dad17d34062469a3aaff80862b787a06265aba
RignonNoel <rignon.noel@openmailbox.org>
2015-11-03 10:52:18 -0500

    Fix the langage by default

---
+++ src/View/Helper/WikiHelper.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Wiki helper
+ */
+class WikiHelper extends Helper
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $defaultConfig = [];
+
+}
