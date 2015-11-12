3654cc4dedc5ecba1636eb5990d3347ef5bae1f3
Simon Begin <simonbegin90@gmail.com>
2015-11-12 11:32:43 -0500

    Create the backend for accept and reject application

---
+++ tests/TestCase/Controller/ApplicationsControllerTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace App\Test\TestCase\Controller;
+
+use App\Controller\ApplicationsController;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * App\Controller\ApplicationsController Test Case
+ */
+class ApplicationsControllerTest extends IntegrationTestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.applications',
+        'app.missions',
+        'app.projects',
+        'app.organizations',
+        'app.organizations_projects',
+        'app.universities',
+        'app.comments',
+        'app.users',
+        'app.hashes',
+        'app.projects_contributors',
+        'app.projects_mentors',
+        'app.type_users',
+        'app.permissions',
+        'app.permissions_type_users',
+        'app.type_users_users',
+        'app.organizations_owners',
+        'app.organizations_members',
+        'app.mission_levels',
+        'app.missions_mission_levels',
+        'app.type_missions',
+        'app.missions_type_missions'
+    ];
+
+    /**
+     * Test index method
+     *
+     * @return void
+     */
+    public function testIndex()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test view method
+     *
+     * @return void
+     */
+    public function testView()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test add method
+     *
+     * @return void
+     */
+    public function testAdd()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test edit method
+     *
+     * @return void
+     */
+    public function testEdit()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+
+    /**
+     * Test delete method
+     *
+     * @return void
+     */
+    public function testDelete()
+    {
+        $this->markTestIncomplete('Not implemented yet.');
+    }
+}
