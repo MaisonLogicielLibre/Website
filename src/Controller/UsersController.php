cd82710bce1b0bf1e30d03c42a8c89edf68c482b
Simon Begin <simonbegin90@gmail.com>
2015-11-24 13:45:08 -0500

    Add collumn uni, org, and Student

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -89,11 +89,45 @@ class UsersController extends AppController
      */
     public function index()
     {
-        $data = $this->DataTables->find('users', ['fields' => ['id', 'username', 'firstName', 'lastName']]);
-
+        $data = $this->DataTables->find(
+            'Users',
+            [
+                'contain' =>
+                    [
+                        'Universities' =>
+                            [
+                                'fields' =>
+                                [
+                                    'id',
+                                    'name'
+                                ]
+                            ],
+                            'Owners' =>
+                            [
+                            'fields' =>
+                            [
+                                'OrganizationsOwners.user_id',
+                                'id',
+                                'name'
+                            ]
+                            ]
+                    ],
+                    'fields' =>
+                    [
+                        'id',
+                        'username',
+                        'firstName',
+                        'lastName',
+                        'isStudent',
+                        'universitie_id'
+                    ]
+            ]
+        );
         $this->set(
             [
                 'data' => $data,
+                'universities' => $this->Users->Universities->find('list', ['limit' => 200]),
+                'orgs' => $this->loadModel('Organizations')->find('list', ['limit' => 200, 'conditions' => ['isRejected' => 0, 'isValidated' => 1]]),
                 '_serialize' => array_merge($this->viewVars['_serialize'], ['data'])
             ]
         );
--- webroot/js/users/index.js
+++ webroot/js/users/index.js
@@ -8,6 +8,27 @@ $(document).ready(function () {
                     text: aData['username']
                 })
             );
+            $('td:eq(3)', nRow).html(
+                $('<span />', {
+                    text: (aData.university ? aData.university.name : notSpecifiedTr)
+                })
+            );
+            var htmlOrg = '';
+            $(aData.owners).each(function (i, v) {
+                htmlOrg += $('<a/>', {
+                    href: orgUrl + '/' + v.id,
+                    text: v.name
+                })[0].outerHTML;
+                htmlOrg += ', ';
+            });
+            $('td:eq(4)', nRow).html(
+                htmlOrg.slice(0, -2) //Remove the (last) comma
+            );
+            $('td:eq(5)', nRow).html(
+                $('<span />', {
+                    text: (aData.isStudent ? yesTr : noTr)
+                })
+            );
         }
     });
 });
