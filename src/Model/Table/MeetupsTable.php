0b7ad7d6ef8dd58b8785d0058e97145d8ea311b2
RignonNoel <rignon.noel@openmailbox.org>
2015-11-25 16:38:32 -0500

    Init model of meetups

---
+++ src/Model/Table/MeetupsTable.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Meetup;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Meetups Model
+ *
+ */
+class MeetupsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->table('meetups');
+        $this->displayField('name');
+        $this->primaryKey('id');
+
+        $this->addBehavior('Timestamp');
+
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('name', 'create')
+            ->notEmpty('name');
+
+        $validator
+            ->requirePresence('description', 'create')
+            ->notEmpty('description');
+
+        $validator
+            ->add('date', 'valid', ['rule' => 'datetime'])
+            ->requirePresence('date', 'create')
+            ->notEmpty('date');
+
+        return $validator;
+    }
+}
