9f45d73befa258b50473f37255e7f5e68b180ff8
raphaelstarnaud <am21830@ens.etsmtl.ca>
2015-12-03 11:33:14 -0500

    Standard

--- src/Controller/MissionsController.php
+++ src/Controller/MissionsController.php
@@ -219,9 +219,9 @@ class MissionsController extends AppController
                 'contain' => ['Projects', 'TypeMissions', 'MissionLevels', 'Applications']
             ]
         );
-		
-		$applications = TableRegistry::get('Applications');
-		
+        
+        $applications = TableRegistry::get('Applications');
+        
         $isEditable = true;
         $minInternNbr = 0;
         foreach ($mission->applications as $application) {
@@ -233,19 +233,18 @@ class MissionsController extends AppController
         if ($minInternNbr == 0) {
             $minInternNbr = 1;
         }
-		
-		if($minInternNbr === $mission->getInternNbr()) {
-			foreach ($mission->applications as $application) {
-				if ($application->accepted != true) {
-					$application->editRejected(true);
-					$applications->save($application);
-				}
-			}
-		}
 
         if ($this->request->is(['patch', 'post', 'put'])) {
             if ($isEditable) {
                 $this->Missions->patchEntity($mission, $this->request->data);
+                if ($minInternNbr === $mission->getInternNbr()) {
+                    foreach ($mission->applications as $application) {
+                        if ($application->accepted != true) {
+                            $application->editRejected(true);
+                            $applications->save($application);
+                        }
+                    }
+                }
             } else {
                 $mission->internNbr = $this->request->data['internNbr'];
             }
