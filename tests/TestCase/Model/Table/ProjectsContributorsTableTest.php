a228414bb9824eac56f320cfe4d31db7f4f6776f
raphaelstarnaud <am21830@ens.etsmtl.ca>
2015-12-18 11:18:12 -0500

    Added tests

---
+++ tests/TestCase/Model/Table/ProjectsContributorsTableTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Tests for ProjectsMentorsTable
+ *
+ * @category Test
+ * @package  Website
+ * @author   Noël Rignon <rignon.noel@openmailbox.org>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GPL v3
+ * @link     https://github.com/MaisonLogicielLibre/Website
+ */
+namespace App\Test\TestCase\Model\Table;
+
+use App\Model\Table\ProjectsContributorsTable;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\TableRegistry;
+use Cake\TestSuite\TestCase;
+use Cake\Validation\Validator;
+
+/**
+ * Tests for ProjectsMentorsTable
+ *
+ * @category Test
+ * @package  Website
+ * @author   Noël Rignon <rignon.noel@openmailbox.org>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GPL v3
+ * @link     https://github.com/MaisonLogicielLibre/Website
+ */
+class ProjectsContributorsTableTest extends TestCase
+{
+
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
+    public $fixtures = [
+        'app.projects',
+        'app.missions',
+        'app.applications',
+        'app.users',
+        'app.universities',
+        'app.comments',
+        'app.projects_contributors',
+        'app.projects_mentors',
+        'app.organizations',
+        'app.organizations_projects',
+        'app.type_users',
+        'app.permissions',
+        'app.permissions_type_users',
+        'app.type_users_users',
+    ];
+
+    /**
+     * SetUp method
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $config = TableRegistry::exists('ProjectsContributors') ? [] : ['className' => 'App\Model\Table\ProjectsContributorsTable'];
+        $this->ProjectsContributors = TableRegistry::get('ProjectsContributors', $config);
+    }
+
+    /**
+     * TearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        unset($this->ProjectsContributors);
+
+        parent::tearDown();
+    }
+
+    /**
+     * Test validation
+     * @return void
+     */
+    public function testValidation()
+    {
+        $validator = new Validator();
+
+        $expected = $validator;
+
+        $result = $this->ProjectsContributors->validationDefault($validator);
+
+        $this->assertEquals($validator, $result);
+    }
+
+    /**
+     * Test buildRules
+     * @return void
+     */
+    public function testBuildRules()
+    {
+        $rule = new RulesChecker();
+
+        $expected = $rule;
+
+        $result = $this->ProjectsContributors->buildRules($rule);
+
+        $this->assertEquals($expected, $result);
+    }
+}
