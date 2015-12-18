753642f23d2bed53f648cd208cb657ef65647ea3
raphaelstarnaud <am21830@ens.etsmtl.ca>
2015-12-18 14:37:34 -0500

    Add tests

--- tests/TestCase/Controller/NewsControllerTest.php
+++ tests/TestCase/Controller/NewsControllerTest.php
@@ -31,56 +31,197 @@ class NewsControllerTest extends IntegrationTestCase
      * @var array
      */
     public $fixtures = [
-        'app.news'
+        'app.news',
+		'app.users',
+		'app.permissions',
+        'app.permissions_type_users',
+		'app.type_users',
+		'app.type_users_users'
     ];
 
     /**
-     * Test index method
+     * Test index - Ok
      *
      * @return void
      */
-    public function testIndex()
+    public function testIndexOk()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->session(['Auth.User.id' => 1]);
+
+        $this->get('/news');
+        $this->assertResponseOk();
+    }
+    
+    /**
+     * Test index - No Auth
+     *
+     * @return void
+     */
+    public function testIndexNoAuth()
+    {
+        $this->get('/news');
+        $this->assertRedirect(['controller' => 'Users', 'action' => 'login']);
+    }
+    
+    /**
+     * Test index - No Perm
+     *
+     * @return void
+     */
+    public function testIndexNoPerm()
+    {
+        $this->session(['Auth.User.id' => 4]);
+
+        $this->get('/news');
+        $this->assertResponseSuccess();
     }
 
     /**
-     * Test view method
+     * Test edit - Ok
      *
      * @return void
      */
-    public function testView()
+    public function testEditOk()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->session(['Auth.User.id' => 1]);
+		
+		$data = [
+            'name' => 'amet',
+            'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'date' => '2015-11-26 16:23:46',
+            'link' => 'Lorem ipsum dolor sit amet'
+		];
+
+        $this->get('/news/edit/1');
+		$this->post('/news/edit/1', $data);
+        $this->assertRedirect(['controller' => 'News', 'action' => 'index']);
+    }
+    
+    /**
+     * Test edit - No Auth
+     *
+     * @return void
+     */
+    public function testEditNoAuth()
+    {
+        $this->get('/news/edit/1');
+        $this->assertRedirect(['controller' => 'Users', 'action' => 'login']);
+    }
+    
+    /**
+     * Test edit - No Perm
+     *
+     * @return void
+     */
+    public function testEditNoPerm()
+    {
+        $this->session(['Auth.User.id' => 4]);
+
+        $this->get('/news/edit/1');
+        $this->assertResponseSuccess();
     }
 
     /**
-     * Test add method
+     * Test add - Ok
      *
      * @return void
      */
-    public function testAdd()
+    public function testAddOk()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->session(['Auth.User.id' => 1]);
+		
+		$data = [
+            'name' => 'Lorem ipsum dolor sit amet',
+            'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'date' => '2015-11-26 16:23:46',
+            'link' => 'Lorem ipsum dolor sit amet'
+		];
+		
+        $this->get('/news/add');
+		$this->post('/news/add', $data);
+        $this->assertRedirect(['controller' => 'News', 'action' => 'index']);
     }
+	
+	/**
+     * Test add - No
+     *
+     * @return void
+     */
+    public function testAddNo()
+    {
+        $this->session(['Auth.User.id' => 1]);
+		
+		$data = [
+            'name' => 'Lorem ipsum dolor sit amet',
+            'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+            'date' => '2015-11-26 16:23:46',
+            'link' => 'Lorem ipsum dolor sit amet'
+		];
+		
+        $this->get('/news/add');
+		$this->post('/news/add', $data);
+        $this->assertResponseSuccess();
+    }
+    
+    /**
+     * Test add - No Auth
+     *
+     * @return void
+     */
+    public function testAddNoAuth()
+    {
+        $this->get('/news/add');
+        $this->assertRedirect(['controller' => 'Users', 'action' => 'login']);
+    }
+    
+    /**
+     * Test add - No Perm
+     *
+     * @return void
+     */
+    public function testAddNoPerm()
+    {
+        $this->session(['Auth.User.id' => 4]);
+
+        $this->get('/news/add');
+        $this->assertResponseSuccess();
+    }
+     
 
     /**
-     * Test edit method
+     * Test delete - Ok
      *
      * @return void
      */
-    public function testEdit()
+    public function testDeleteOk()
     {
-        $this->markTestIncomplete('Not implemented yet.');
-    }
+        $this->session(['Auth.User.id' => 1]);
 
+        $this->get('/news/delete/1');
+        $this->assertResponseSuccess();
+    }
+    
     /**
-     * Test delete method
+     * Test delete - No Auth
      *
      * @return void
      */
-    public function testDelete()
+    public function testDeleteNoAuth()
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->get('/news/delete/1');
+        $this->assertRedirect(['controller' => 'Users', 'action' => 'login']);
+    }
+    
+    /**
+     * Test delete - No Perm
+     *
+     * @return void
+     */
+    public function testDeleteNoPerm()
+    {
+        $this->session(['Auth.User.id' => 4]);
+
+        $this->get('/news/delete/1');
+        $this->assertResponseSuccess();
     }
 }
