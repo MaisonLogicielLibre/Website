fff9ac5fc3558b8233857e9bdc9707c1a167ee8a
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-26 15:18:14 -0500

    fix(tests): Remove UsersController#add

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -207,35 +207,6 @@ class UsersController extends AppController
     }
 
     /**
-     * Add method
-     *
-     * @return redirect
-     */
-    public function add()
-    {
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->data);
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(
-                    __(
-                        'The new email could not be saved. Please,
-                try again.'
-                    )
-                );
-            }
-        }
-        $typeUsers = $this->Users->TypeUsers->find('list', ['limit' => 200]);
-        $universities = $this->Users->Universities->find('list', ['limit' => 200]);
-        $projects = $this->Users->Projects->find('list', ['limit' => 200]);
-        $this->set(compact('user', 'typeUsers', 'universities', 'projects'));
-        $this->set('_serialize', ['user']);
-    }
-
-    /**
      * Register method
      *
      * @return redirect
