4f892eae89daf64845e04d3ff7caa3a34090976b
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-01-27 11:35:08 -0500

    feat(migrations): CreateUsersTypeMissions

---
+++ config/Migrations/20160127163040_create_users_type_missions.php
@@ -0,0 +1,43 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class CreateUsersTypeMissions extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $table = $this->table('users_type_missions');
+        $table->addColumn('user_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false
+        ]);
+        $table->addColumn('type_mission_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false
+        ]);
+        $table->create();
+    }
+}
