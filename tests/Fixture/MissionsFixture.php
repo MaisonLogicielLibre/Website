23a483bb6761c88e139ce6dfad53c079fbe7444f
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-02-29 11:42:08 -0500

    fix(test): No null session in fixtures

--- tests/Fixture/MissionsFixture.php
+++ tests/Fixture/MissionsFixture.php
@@ -98,7 +98,7 @@ class MissionsFixture extends TestFixture
         [
             'id' => 4,
             'name' => 'Lorem ipsum dolor sit amet',
-            'session' => null,
+            'session' => 0,
             'length' => null,
             'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
             'competence' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
