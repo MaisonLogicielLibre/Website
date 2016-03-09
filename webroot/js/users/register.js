cc56da62720911c9661b0a3d2306ddbbdc3c3b4c
Felix Leblanc <felix.leblanc1305@gmail.com>
2016-03-09 09:36:10 -0500

    fix(terms): Remove console logs

--- webroot/js/users/register.js
+++ webroot/js/users/register.js
@@ -11,11 +11,8 @@ $(document).ready(function () {
     $('.form-horizontal').submit(function(e) {
       checked = $('.terms-and-conditions-container :checkbox').attr("checked");
       if(checked == "checked") {
-        console.log('Submitting');
         $(this).unbind('submit').submit();
       } else {
-        console.log(checked);
-        console.log('The value is not checked yet');
         e.preventDefault();
         $('.form-horizontal div').hide();
         $('.terms-and-conditions-container').show();
