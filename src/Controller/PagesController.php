751ce288197dcd01f27ff78d5fa0ec5adb9a6e8d
willkoua <willkoua@gmail.com>
2016-05-20 01:20:43 -0400

    reglement de conflits

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -606,7 +606,6 @@ class PagesController extends AppController
                             }
                         } else {
                             $this->Flash->error(__('path specified not valid'), 'er_gene');
-
                         }
                     } else {
                         $this->Flash->error(__('image file size incorrect'), 'er_gene');
